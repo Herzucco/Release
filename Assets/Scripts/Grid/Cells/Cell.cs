09225ff2bd3bfbb680d6437677e7bcb58a29f0f9
Herzucco <a.carta@student.isartdigital.com>
2015-04-01 15:52:22 +0200

    commit before le merge

--- Assets/Scripts/Grid/Cells/Cell.cs
+++ Assets/Scripts/Grid/Cells/Cell.cs
@@ -3,16 +3,22 @@ using System.Collections;
 using DG.Tweening;
 
 public class Cell : BaseObject {
+	[HideInInspector]
 	public int index;
+	[HideInInspector]
 	public int colorIndex;
+	[HideInInspector]
 	public Columns column;
 
+	[HideInInspector]
 	public Vector2 basePosition;
+	[HideInInspector]
 	public Vector2 baseScale;
 
 	protected SpriteRenderer sRenderer;
 	protected CircleCollider2D cCollider;
 	protected CellSettings settings;
+	protected bool isListening;
 
 	protected virtual void Awake(){
 		sRenderer = GetComponent<SpriteRenderer> ();
@@ -41,6 +47,11 @@ public class Cell : BaseObject {
 
 		//do fall
 		transform.DOScale (baseScale, settings.scaleAnimationDuration).SetEase (settings.scaleAnimationEase);
+	}
+
+	public virtual void ListenSelection(){
+		isListening = true;
+
 	}
 
 	public virtual void ChangeColor(Color color){
